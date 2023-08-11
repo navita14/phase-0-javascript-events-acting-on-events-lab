83b0fc4d8f206d5734187ef8acf02337e26f051f
Navita Ramasar <navitaramasar14@gmail.com>
2023-08-11 16:24:43 -0400

    Left and Right JS

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+//   function moveDodgerRight() {
+//     const leftNumbers = dodger.style.right.replace("px", "");
+//     const right = parseInt(leftNumbers, 179);
+  
+//     if (right > 179) {
+//       dodger.style.right = `${left + 1}px`;
+//     }
+//   }
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if ( left < 360) {
+      dodger.style.left = `${left + 4}px`;
+    }
+  }
